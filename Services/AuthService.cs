bfee34af99bb9cfce89175ef58bd8437da283915
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-09-30 15:28:34 +0200

    Added Auth / User services

---
+++ Services/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using Playmor_Asp.DTOs;
+using Playmor_Asp.Interfaces;
+
+namespace Playmor_Asp.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IHashingService _hashingService;
+        public AuthService(IUserRepository userRepository, IHashingService hashingService) 
+        {
+            _userRepository = userRepository;
+            _hashingService = hashingService;
+        }
+        public string Login(UserLoginDTO userLoginDTO)
+        {
+            var user = _userRepository.GetByUsername(userLoginDTO.Username);
+            if (user == null)
+                throw new Exception($"User with name {userLoginDTO.Username} not found");
+
+            var hashesMatch = _hashingService.CompareHash(userLoginDTO.Password, user.PasswordHash, user.PasswordSalt);
+            if (!hashesMatch)
+                throw new Exception("Invalid credentials");
+
+            var token = CreateToken();
+            return token;
+        }
+
+        public string Register(UserRegisterDTO userRegisterDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/HashingService.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace Playmor_Asp.Services
+{
+    public class HashingService
+    {
+        public Tuple<byte[], byte[]> CreateHash(string password)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                var salt = hmac.Key;
+                var hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return new Tuple<byte[], byte[]>(hash, salt);
+            }
+        }
+        public bool CompareHash(string password, byte[] hash, byte[] salt)
+        {
+            using (var hmac = new HMACSHA512(salt))
+            {
+                var newHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return hash.SequenceEqual(newHash);
+            }
+        }
+    }
+}
