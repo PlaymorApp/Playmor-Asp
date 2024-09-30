1dbc106abb6f203bcd90633570c2551569650efa
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-09-30 15:29:20 +0200

    Created a mapper

---
+++ Mapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Playmor_Asp.DTOs;
+using Playmor_Asp.Models;
+
+namespace Playmor_Asp.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+            CreateMap<UserUpdateDTO, User>();
+            CreateMap<User, UserUpdateDTO>();
+        }
+    }
+}
