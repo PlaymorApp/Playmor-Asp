fd0d71b76eb844a3d625313178b0592835e3c97e
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-02 14:57:58 +0200

    New User DTOs

--- DTOs/UserUpdateDTO.cs
+++ DTOs/UserUpdateDTO.cs
@@ -2,5 +2,8 @@
 {
     public class UserUpdateDTO
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
