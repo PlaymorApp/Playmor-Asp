5bb5822b6987e9358873dcc88a2b532d45612725
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-02 18:09:21 +0200

    Namespaces: Block -> File scoped.

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,57 +3,56 @@ using Playmor_Asp.DTOs;
 using Playmor_Asp.Interfaces;
 using Playmor_Asp.Models;
 
-namespace Playmor_Asp.Services
-{
-    public class UserService : IUserService
-    {
-        private readonly IUserRepository _userRepository;
-        private readonly IMapper _mapper;
-        public UserService(IMapper mapper, IUserRepository userRepository)
-        { 
-            _userRepository = userRepository;
-            _mapper = mapper;
-        }
-        public ICollection<UserDTO> GetAllUsers()
-        {
-            var users = _userRepository.GetAll();
-            return _mapper.Map<ICollection<UserDTO>>(users);
-        }
+namespace Playmor_Asp.Services;
 
-        public UserDTO GetUserById(int id)
-        {
-            if (id < 1) throw new ArgumentException($"Invalid id argument, passed id: {id}. Must be greater than 1");
-            var user = _userRepository.GetByID(id);
-            if (user == null) throw new Exception($"User with ${id} id not found");
-            return _mapper.Map<UserDTO>(user);
-        }
-        
-        public UserDTO GetUserByEmail(string email)
-        {
-            var user = _userRepository.GetByEmail(email);
-            if (user == null) throw new Exception($"User with ${email} email not found");
-            return _mapper.Map<UserDTO>(user);
-        }
-        
-        public UserDTO GetUserByUsername(string username)
-        {
-            var user = _userRepository.GetByUsername(username);
-            if (user == null) throw new Exception($"User with ${username} username not found");
-            return _mapper.Map<UserDTO>(user);
-        }
-        
-        public UserUpdateDTO UpdateUser(int id, UserUpdateDTO userDTO)
-        {
-            if (id < 1) throw new ArgumentException($"Invalid argument {id}. Must be greater or equal 1");
-            _ = _userRepository.GetByID(id) ?? throw new Exception($"User with id {id} not found");
-            var mappedUser = _mapper.Map<User>(userDTO);
-            var updatedUser = _userRepository.Update(id, mappedUser, typeof(UserUpdateDTO));
-            return _mapper.Map<UserUpdateDTO>(updatedUser);
-        }
-        
-        public UserDTO DeleteUser(int id)
-        {
-            throw new NotImplementedException();
-        }
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+    public UserService(IMapper mapper, IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+    public ICollection<UserDTO> GetAllUsers()
+    {
+        var users = _userRepository.GetAll();
+        return _mapper.Map<ICollection<UserDTO>>(users);
+    }
+
+    public UserDTO GetUserById(int id)
+    {
+        if (id < 1) throw new ArgumentException($"Invalid id argument, passed id: {id}. Must be greater than 1");
+        var user = _userRepository.GetByID(id);
+        if (user == null) throw new Exception($"User with ${id} id not found");
+        return _mapper.Map<UserDTO>(user);
+    }
+
+    public UserDTO GetUserByEmail(string email)
+    {
+        var user = _userRepository.GetByEmail(email);
+        if (user == null) throw new Exception($"User with ${email} email not found");
+        return _mapper.Map<UserDTO>(user);
+    }
+
+    public UserDTO GetUserByUsername(string username)
+    {
+        var user = _userRepository.GetByUsername(username);
+        if (user == null) throw new Exception($"User with ${username} username not found");
+        return _mapper.Map<UserDTO>(user);
+    }
+
+    public UserUpdateDTO UpdateUser(int id, UserUpdateDTO userDTO)
+    {
+        if (id < 1) throw new ArgumentException($"Invalid argument {id}. Must be greater or equal 1");
+        _ = _userRepository.GetByID(id) ?? throw new Exception($"User with id {id} not found");
+        var mappedUser = _mapper.Map<User>(userDTO);
+        var updatedUser = _userRepository.Update(id, mappedUser, typeof(UserUpdateDTO));
+        return _mapper.Map<UserUpdateDTO>(updatedUser);
+    }
+
+    public UserDTO DeleteUser(int id)
+    {
+        throw new NotImplementedException();
     }
 }
