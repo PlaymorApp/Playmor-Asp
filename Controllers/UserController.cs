426c9642de508fa0de495c82bec7ba797f4a1c01
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-02 14:58:57 +0200

    User and auth controllers done

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Playmor_Asp.DTOs;
+using Playmor_Asp.Interfaces;
+using Playmor_Asp.Models;
+
+namespace Playmor_Asp.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("users/{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Authorize]
+        public IActionResult GetUser(int userId)
+        {
+            try
+            {
+                var user = _userService.GetUserById(userId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                if (!ModelState.IsValid) { return BadRequest(ModelState); }
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
