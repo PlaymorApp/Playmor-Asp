54adba58054ef6d823623695b7b204cf5a49eb64
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-07 17:16:35 +0200

    Migrations for new game model

---
+++ Migrations/20241007132734_AddFieldsToGameModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Playmor_Asp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldsToGameModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Website",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WebsiteName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WebsiteLink = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Website", x => new { x.GameId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Website_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Website");
+
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Games");
+        }
+    }
+}
