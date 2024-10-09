e93f96eff48211b59f20134154bf0041832d78b4
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-09 18:07:06 +0200

    Added created at dates to every model + corresponding migrations

--- Domain/Models/Game.cs
+++ Domain/Models/Game.cs
@@ -1,37 +1,52 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Playmor_Asp.Domain.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Playmor_Asp.Domain.Models;
 
 public class Game
 {
     public int Id { get; set; }
+
     [Required]
     public required string Title { get; set; }
+
     [Required]
     public required string Description { get; set; }
+
     [Required]
     public required string Details { get; set; }
+
     [Required]
     public required List<string> Developer { get; set; }
+
     [Required]
     public required List<string> Publisher { get; set; }
+
     [Required]
     public required List<string> Platforms { get; set; }
+
     [Required]
     public required List<string> Genres { get; set; }
+
     [Required]
     public required List<string> Modes { get; set; }
+
     [Required]
     public required string Cover { get; set; }
+
     [Required]
     public required string Artwork { get; set; }
+
     [Required]
     public required List<ReleaseDate> ReleaseDates { get; set; }
+
     [Required]
     public required List<Website> WebsiteLinks { get; set; }
 
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
 
 [Owned]
--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,17 +1,35 @@
 ﻿using Playmor_Asp.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Playmor_Asp.Domain.Models;
 
 public class User
 {
     public int Id { get; set; }
+
+    [Required]
     public required string Username { get; set; }
+
+    [Required]
     public required byte[] PasswordHash { get; set; }
+
+    [Required]
     public required byte[] PasswordSalt { get; set; }
+
     public string Token { get; set; }
+
     public DateTime TokenCreated { get; set; }
+
     public DateTime TokenExpires { get; set; }
+
+    [Required]
     public required string Email { get; set; }
-    public required string PhoneNumber { get; set; }
+
+    public string PhoneNumber { get; set; }
+
     public UserRole UserRole { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
