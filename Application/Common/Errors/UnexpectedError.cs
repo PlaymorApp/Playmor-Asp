dc96b636c4e5af351f3b73cf643ad1f6347f90a0
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-16 20:49:09 +0200

    Added a 500 unexpected error

---
+++ Application/Common/Errors/UnexpectedError.cs
@@ -0,0 +1,13 @@
+﻿namespace Playmor_Asp.Application.Common.Errors;
+
+public class UnexpectedError : IError
+{
+    public string Message { get; }
+    public string ErrorCode { get; }
+
+    public UnexpectedError(string message)
+    {
+        Message = message;
+        ErrorCode = "Unexpected server error";
+    }
+}
