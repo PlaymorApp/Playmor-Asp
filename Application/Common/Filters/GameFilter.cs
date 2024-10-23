50e5061b051f72c4315e0974cfa15856cc6ab8d3
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-23 19:42:20 +0200

    Added keyword to game filter

--- Application/Common/Filters/GameFilter.cs
+++ Application/Common/Filters/GameFilter.cs
@@ -5,6 +5,7 @@ namespace Playmor_Asp.Application.Common.Filters;
 public class GameFilter
 {
     public DateRange? DateRange { get; set; }
+    public string? Keyword { get; set; }
     public ICollection<string>? Genres { get; set; }
     public ICollection<string>? Modes { get; set; }
     public ICollection<string>? Developers { get; set; }
