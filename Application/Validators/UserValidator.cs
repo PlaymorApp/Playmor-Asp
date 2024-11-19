e25632c733e637ddb7113c9d39cb4adb283abd26
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-19 14:31:52 +0100

    Implemented rules for userValidator

--- Application/Validators/UserValidator.cs
+++ Application/Validators/UserValidator.cs
@@ -1,5 +1,32 @@
-﻿namespace Playmor_Asp.Application.Validators;
+﻿using FluentValidation;
+using Playmor_Asp.Domain.Enums;
+using Playmor_Asp.Domain.Models;
 
-public class UserValidator
+namespace Playmor_Asp.Application.Validators;
+
+public class UserValidator : AbstractValidator<User>
 {
+    public UserValidator()
+    {
+        RuleFor(user => user.Id)
+           .GreaterThanOrEqualTo(0)
+           .WithMessage("ID must be the greater than or equal to 0");
+
+        RuleFor(user => user.Username)
+           .NotEmpty()
+           .MaximumLength(100);
+
+        RuleFor(user => user.Email)
+            .NotEmpty()
+            .MaximumLength(100)
+            .EmailAddress();
+
+        RuleFor(user => user.PhoneNumber)
+            .MaximumLength(9);
+
+        RuleFor(user => user.UserRole)
+            .IsInEnum().WithMessage("Invalid user role.")
+            .Must(role => role == UserRole.User || role == UserRole.Admin)
+            .WithMessage("User role must be either User or Admin.");
+    }
 }
