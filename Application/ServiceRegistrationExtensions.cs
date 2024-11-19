401acce45b4cf5b7a41de89ca7d15c7024d3b908
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-19 14:33:04 +0100

    DI injection for usergameservice, usergamerepository, uservalidator, usergamevalidator

--- Application/ServiceRegistrationExtensions.cs
+++ Application/ServiceRegistrationExtensions.cs
@@ -19,10 +19,14 @@ public static class ServiceRegistrationExtensions
         services.AddScoped<IGameRepository, GameRepository>();
         services.AddScoped<IGameService, GameService>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserGameService, UserGameService>();
+        services.AddScoped<IUserGameRepository, UserGameRepository>();
         services.AddScoped<IHashingService, HashingService>();
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IValidator<Game>, GameValidator>();
+        services.AddScoped<IValidator<User>, UserValidator>();
+        services.AddScoped<IValidator<UserGame>, UserGameValidator>();
         services.AddAutoMapper(typeof(Program));
         return services;
     }
