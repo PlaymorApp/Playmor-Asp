08607729a181236bbb07ad18f3742bee246a358b
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-19 11:53:36 +0100

    Added UserGame repository and service interfaces

---
+++ Application/Interfaces/IUserGameRepository.cs
@@ -0,0 +1,12 @@
+﻿using Playmor_Asp.Domain.Models;
+
+namespace Playmor_Asp.Application.Interfaces;
+
+public interface IUserGameRepository
+{
+    public Task<UserGame?> GetByIDAsync(int id);
+    public Task<ICollection<UserGame>> GetByUserIDAsync(int userId);
+    public Task<UserGame?> CreateAsync(UserGame userGame);
+    public Task<bool> DeleteAsync(int id);
+    public Task<bool> SaveAsync();
+}
---
+++ Application/Interfaces/IUserGameService.cs
@@ -0,0 +1,14 @@
+﻿using Playmor_Asp.Application.Common;
+using Playmor_Asp.Application.Common.Errors;
+using Playmor_Asp.Application.DTOs;
+
+namespace Playmor_Asp.Application.Interfaces;
+
+public interface IUserGameService
+{
+    public Task<ServiceResult<UserGameDTO?, IError>> GetUserGameByIdAsync(int id);
+    public Task<ServiceResult<ICollection<UserGameDTO>, IError>> GetUserGamesByUserIdAsync(int userId);
+    public Task<ServiceResult<bool, IError>> DeleteUserGameAsync(int id);
+    public Task<ServiceResult<bool, IError>> CreateUserGameAsync(UserGameDTO userGameDTO);
+
+}
