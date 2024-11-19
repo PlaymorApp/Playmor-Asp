ca2217d6380526e3fc2d5dfb83ee653b6672447b
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-19 11:52:48 +0100

    UserDTO now also has CreatedAt field

--- Application/DTOs/UserDTO.cs
+++ Application/DTOs/UserDTO.cs
@@ -9,4 +9,6 @@ public class UserDTO
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public UserRole UserRole { get; set; }
+
+    public DateTime CreatedAt { get; set; }
 }
