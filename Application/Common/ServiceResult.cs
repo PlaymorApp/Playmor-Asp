c09d8177d383d30664df3ef5de14c6b3cd1e3b87
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-19 14:32:12 +0100

    Removed unused properties in service result

--- Application/Common/ServiceResult.cs
+++ Application/Common/ServiceResult.cs
@@ -3,8 +3,6 @@
 public class ServiceResult<T, TError>
 {
     public List<TError> Errors = [];
-    public List<string> Warnings = [];
     public bool IsValid => Errors.Count == 0;
-    public bool HasWarnings => Warnings.Count != 0;
     public required T Data { get; set; }
 }
