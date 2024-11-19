d3a02d7bc9428b488933de29dd1d1968c23d5a9d
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-19 11:52:28 +0100

    UserGame DB Migrations

---
+++ Infrastructure/Migrations/20241118205638_CreateUserGames.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Playmor_Asp.Migrations;
+
+/// <inheritdoc />
+public partial class CreateUserGames : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "UserGames",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                GameId = table.Column<int>(type: "int", nullable: false),
+                UserId = table.Column<int>(type: "int", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserGames", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_UserGames_Games_GameId",
+                    column: x => x.GameId,
+                    principalTable: "Games",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_UserGames_Users_UserId",
+                    column: x => x.UserId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            name: "IX_UserGames_GameId",
+            table: "UserGames",
+            column: "GameId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_UserGames_UserId",
+            table: "UserGames",
+            column: "UserId");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "UserGames");
+    }
+}
