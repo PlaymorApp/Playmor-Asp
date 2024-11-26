a202ecb1a1fb13de5bb5fa084337efad5966db6c
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-26 21:06:00 +0100

    Added UnauthorizedError class

---
+++ Application/Common/Errors/UnauthorizedError.cs
@@ -0,0 +1,13 @@
+﻿namespace Playmor_Asp.Application.Common.Errors;
+
+public class UnauthorizedError : IError
+{
+    public string Message { get; }
+    public string ErrorCode { get; }
+
+    public UnauthorizedError(string message)
+    {
+        Message = message;
+        ErrorCode = "Unauthorized action error";
+    }
+}
