5db882c4f4be4111b83314aef2227ddd27b7735f
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-26 21:04:56 +0100

    Expansion to message module

---
+++ Application/Interfaces/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Playmor_Asp.Domain.Models;
+
+namespace Playmor_Asp.Application.Interfaces;
+
+public interface IMessageRepository
+{
+    public Task<Message?> GetByIDAsync(int id);
+    public Task<ICollection<Message>> GetByRecipientIdAsync(int recipientId);
+    public Task<ICollection<Message>> GetBySenderIdAsync(int senderId);
+    public Task<Message?> CreateAsync(Message message);
+    public Task<bool> DeleteAsync(int id);
+    public Task<bool> SaveAsync();
+}
