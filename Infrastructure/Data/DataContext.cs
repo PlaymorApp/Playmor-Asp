2a3d3dd408aa700144430a27af2e0a8456778648
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-26 21:01:41 +0100

    Updated data context with new collections

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -10,6 +10,8 @@ public class DataContext : DbContext
     public DbSet<Game> Games { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<UserGame> UserGames { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Comment> Comments { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Game>()
