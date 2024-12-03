499771036bce1f37a06c125f3bb987d453bcefcb
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-12-03 01:08:30 +0100

    Added Helper functions

---
+++ Helpers/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Playmor_Asp.Helpers;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int? GetUserId(this ClaimsPrincipal user)
+    {
+        var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return int.TryParse(userIdClaim, out var userId) ? userId : null;
+    }
+}
---
+++ Helpers/PropertyCopier.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Playmor_Asp.Helpers;
+
+public class PropertyCopier
+{
+    public static void CopyProperties<T>(T source, T destination, string[]? excludedProperties = null) where T : class
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (destination == null) throw new ArgumentNullException(nameof(destination));
+
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                  .Where(prop => prop.CanRead && prop.CanWrite);
+
+        excludedProperties ??= [];
+
+        foreach (var property in properties)
+        {
+            if (excludedProperties.Contains(property.Name, StringComparer.CurrentCultureIgnoreCase))
+                continue;
+
+            var value = property.GetValue(source);
+            property.SetValue(destination, value);
+        }
+    }
+}
