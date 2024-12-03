f32d905562ce1c84e55fc7074b6071d442048504
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-12-03 01:05:24 +0100

    New Migrations

---
+++ Infrastructure/Migrations/20241128142032_AddGameIdToCommentEntity.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Playmor_Asp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameIdToCommentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_CommenterId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_CommenterId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GameId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "Comments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CommenterId",
+                table: "Comments",
+                column: "CommenterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_CommenterId",
+                table: "Comments",
+                column: "CommenterId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
