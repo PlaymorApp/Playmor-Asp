c16931fb9f23492f75229da6b542dbf7ffbdfcd4
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-12-08 18:07:51 +0100

    CommentController finished

--- Presentation/Controllers/CommentController.cs
+++ Presentation/Controllers/CommentController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Playmor_Asp.Application.Common.Errors;
 using Playmor_Asp.Application.DTOs.Comment;
 using Playmor_Asp.Application.Interfaces;
+using Playmor_Asp.Helpers;
 
 namespace Playmor_Asp.Presentation.Controllers;
 [Route("api")]
@@ -79,8 +80,118 @@ public class CommentController : Controller
         return Ok(serviceResult.Data);
     }
 
-    // TODO:
-    // Create
-    // Update
-    // Delete
+    [HttpPost("/comments")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CommentDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateCommentAsync([FromBody] CommentPostDTO commentPostDTO)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        if (User.GetUserId() is not int userId)
+        {
+            return Unauthorized("Unauthorized to access resource");
+        }
+
+        var serviceResult = await _commentService.CreateCommentAsync(commentPostDTO, userId);
+
+        if (!serviceResult.IsValid)
+        {
+            if (serviceResult.Errors.Any(e => e is UnauthorizedError))
+            {
+                return Unauthorized("Unauthorized Request.");
+            }
+
+            if (serviceResult.Errors.Any(e => e is ValidationError))
+            {
+                return BadRequest(serviceResult.Errors);
+            }
+
+            if (serviceResult.Errors.Any(e => e is UnexpectedError))
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Unexpected server error.");
+            }
+        }
+
+        return Ok(serviceResult.Data);
+    }
+
+    [HttpPut("/comments/{commentId}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CommentDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateCommentAsync([FromRoute] int commentId, [FromBody] CommentPutDTO commentPutDTO)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        if (User.GetUserId() is not int userId)
+        {
+            return Unauthorized("Unauthorized to access resource");
+        }
+
+        var serviceResult = await _commentService.UpdateCommentAsync(commentPutDTO, userId);
+
+        if (!serviceResult.IsValid)
+        {
+            if (serviceResult.Errors.Any(e => e is UnauthorizedError))
+            {
+                return Unauthorized("Unauthorized Request.");
+            }
+
+            if (serviceResult.Errors.Any(e => e is ValidationError))
+            {
+                return BadRequest(serviceResult.Errors);
+            }
+
+            if (serviceResult.Errors.Any(e => e is UnexpectedError))
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Unexpected server error.");
+            }
+        }
+
+        return Ok(serviceResult.Data);
+    }
+
+    [HttpDelete("/comments/{commentId}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> DeleteCommentAsync([FromRoute] int commentId)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        if (User.GetUserId() is not int userId)
+        {
+            return Unauthorized("Unauthorized to access resource");
+        }
+
+        var serviceResult = await _commentService.DeleteCommentAsync(commentId, userId);
+
+        if (!serviceResult.IsValid)
+        {
+            if (serviceResult.Errors.Any(e => e is UnauthorizedError))
+            {
+                return Unauthorized("Unauthorized Request.");
+            }
+
+            if (serviceResult.Errors.Any(e => e is ValidationError))
+            {
+                return BadRequest(serviceResult.Errors);
+            }
+
+            if (serviceResult.Errors.Any(e => e is UnexpectedError))
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Unexpected server error.");
+            }
+        }
+
+        return Ok(serviceResult.Data);
+    }
+
 }
