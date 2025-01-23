b40f73a03b27fd1ec93ffc8a43effccf980e7cf2
Przemysław Kaczmarski <nyviel111@gmail.com>
2025-01-23 17:36:55 +0100

    Made user phone number nullable

--- Application/DTOs/User/UserPostDTO.cs
+++ Application/DTOs/User/UserPostDTO.cs
@@ -5,5 +5,4 @@ public class UserPostDTO
     public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
-    public string PhoneNumber { get; set; }
 }
--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -14,7 +14,7 @@ public class User
     public DateTime TokenCreated { get; set; }
     public DateTime TokenExpires { get; set; }
     public required string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
     public UserRole UserRole { get; set; }
 
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
